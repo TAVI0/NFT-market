f23877226592c9407bdcbbbbb7cf66e1e4f45a93
TAVI0 <marcos_tavio@outlook.com>
2022-01-29 23:00:40 -0300

    clase 15

--- contracts/PlatziPunks.sol
+++ contracts/PlatziPunks.sol
@@ -2,7 +2,57 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "./Base64.sol";
+contract PlatziPunks is ERC721, ERC721Enumerable {
+    using Counters for Counters.Counter;
 
-contract PlatziPunks is ERC721 {
-    constructor() ERC721("PlatziPunks", "PLPKS"){}
+    Counters.Counter private _idCounter;
+    uint256 public maxSupply;
+
+    constructor(uint _maxSupply) ERC721("PlatziPunks", "PLPKS"){
+        maxSupply = _maxSupply;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(_exists(tokenId),"no existe el toquen, gege");
+
+        string memory jsonURI = Base64.encode(
+            abi.encodePacked(
+                '{ "name": "PlatziPunks #', tokenId,
+                '",""Description":  ""Platzi Punks are randomized Avataaars stored on chain to teach DApp development on Platzi", "image: "',
+                "//TODO_ calcular IMG URL"
+                '"}'
+            )
+        );
+
+        return string(abi.encodePacked("data:application/json;base64,", jsonURI));
+    }
+
+    function mint() public{
+        uint256 current = _idCounter.current();
+        require(current < maxSupply, "No PlatziPunks left :(");
+
+        _safeMint(msg.sender, current);
+        _idCounter.increment()
+;
+
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 }
